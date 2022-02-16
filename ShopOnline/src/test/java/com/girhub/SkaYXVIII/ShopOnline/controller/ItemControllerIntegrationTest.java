f734259102dd036fe6f43173237e651fcf65f8ee
BartoszRadtke <bartosz.radtke2001@gmail.com>
2022-02-16 14:30:51 +0100

    Added Post method test

--- ShopOnline/src/test/java/com/girhub/SkaYXVIII/ShopOnline/controller/ItemControllerIntegrationTest.java
+++ ShopOnline/src/test/java/com/girhub/SkaYXVIII/ShopOnline/controller/ItemControllerIntegrationTest.java
@@ -1,18 +1,19 @@
 package com.girhub.SkaYXVIII.ShopOnline.controller;
 
-import com.girhub.SkaYXVIII.ShopOnline.model.Item;
-import com.girhub.SkaYXVIII.ShopOnline.model.ItemRepository;
-import com.girhub.SkaYXVIII.ShopOnline.model.ItemsGroup;
-import com.girhub.SkaYXVIII.ShopOnline.model.ItemsGroupRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.girhub.SkaYXVIII.ShopOnline.model.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,8 +35,6 @@ class ItemControllerIntegrationTest {
     @Test
     void httpGet_readItemById() throws Exception {
         ItemsGroup group = groupRepository.findById(1).orElse(null);
-        System.out.println(group);
-
 
         int id = repository.save(new Item("foo", 5.5f, "boo", group)).getId();
 
@@ -43,5 +42,25 @@ class ItemControllerIntegrationTest {
                 .andExpect(status().is2xxSuccessful())
                 .andDo(print())
                 .andExpect(content().string(containsString("foo")));
+    }
+
+    @Test
+    void httpPost_createItem() throws Exception {
+        ItemForm itemForm = new ItemForm();
+        itemForm.setName("foo");
+        itemForm.setDescription("bar");
+        itemForm.setPrice(5.5f);
+        itemForm.setGroup(1);
+
+        mockMvc.perform(post("/items")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(itemForm)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(containsString("foo")));
+    }
+
+    private String toJson(Object obj) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(obj);
     }
 }
